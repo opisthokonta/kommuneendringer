f2bf4568b02b430fa30f35f51390213843b6a40c
Jonas Christoffer Lindstrøm <JonasChristoffer.Lindstrom@fhi.no>
2025-03-26 15:01:30 +0100

    rewirte of build graph

--- data-raw/04 - build graph.R
+++ data-raw/04 - build graph.R
@@ -1,5 +1,5 @@
 
-
+# New version of build graph: Try to iteratively apply the changetables to initial list of municipalities.
 
 library(fs)
 library(dplyr)
@@ -13,7 +13,7 @@ library(DiagrammeR)
 
 datapath <- path('..', 'data')
 
-FINAL_DATE <- ymd('2034-12-31')
+FINAL_DATE <- ymd('2035-12-31')
 
 
 load(path(datapath, 'changetable.rda'))
@@ -59,86 +59,66 @@ changetable %>%
 
 
 
-# infer the new nodes
-changetable_filtered %>%
-  distinct(newCode, newName, changeOccurred) %>%
-  rename(code = newCode,
-         full_name = newName,
-         start_date = changeOccurred) %>%
-  mutate(NODE = nrow_nodes_init + (1:n()),
-         end_date = FINAL_DATE) -> new_nodes
+# Identify new nodes using the changetable. ----
 
-all_nodes <- bind_rows(nodes_init, new_nodes)
+all_nodes <- nodes_init
+
+change_dates <- sort(unique(changetable_filtered$changeOccurred))
+n_changetables <- length(change_dates)
 
 
-# 4 rows removed by distinct(), for municipalities that were split
-# Tysfjord, Snilfjord.
+for (ii in 1:n_changetables){
+
+  print(change_dates[ii])
+
+  # Current changetable to apply
+  changetable1 <- changetable_filtered %>%
+    filter(changeOccurred == change_dates[ii])
+
+  # Add temporary variable currently_valid to all_nodes.
+  # This will be removed later.
+  all_nodes %>%
+    mutate(currently_valid = end_date >= change_dates[ii]) -> all_nodes
+
+  # Some checks.
+  all_nodes %>%
+    filter(currently_valid) -> all_currently_valid_nodes
+
+  stopifnot(!any(duplicated(all_currently_valid_nodes$code)))
+  stopifnot(nrow(all_currently_valid_nodes) > 0)
 
 
-changetable_filtered %>%
-  distinct(oldCode, oldName, changeOccurred) %>%
-  rename(code = oldCode,
-         full_name = oldName,
-         end_date2 = changeOccurred) %>%
-  mutate(end_date2 = end_date2 - 1) %>%
-  mutate(NODE_UPDATE_ID = 1:n()) -> nodes_to_update
+  # Identify new nodes, make rows to add to node list.
+  changetable1 %>%
+    filter(!duplicated(newCode)) %>%
+    select(newCode, newName, changeOccurred) %>%
+    rename(code = newCode,
+           full_name = newName,
+           start_date = changeOccurred) %>%
+    mutate(end_date = as.Date(FINAL_DATE),
+           NODE = (1:n()) + max(all_nodes$NODE)) -> new_nodes
+
+  # update end_date in all_nodes
+  all_nodes %>%
+    mutate(end_date = if_else(currently_valid & code %in% changetable1$oldCode, as.Date(change_dates[ii]) - 1, end_date)) -> all_nodes
+
+  # Add new nodes to all_nodes
+  all_nodes %>%
+    bind_rows(new_nodes) -> all_nodes
 
 
-
-# Remove Trondheim before updating nodes. These dates are manually added later.
-nodes_to_update %>%
-  filter(!str_detect(full_name, 'Trondheim')) -> nodes_to_update
-
-# Remove Tønsberg before updating nodes. The dates are manually added later.
-# Because of encoding problems, I use a regular expression wildcard for the ø.
-nodes_to_update %>%
-  filter(!str_detect(full_name, 'T.nsberg')) -> nodes_to_update
+}
 
 
-nodes_to_update %>%
-  filter(!str_detect(full_name, 'Ringsaker')) -> nodes_to_update
-
+nrow(all_nodes)
 
 all_nodes %>%
-  left_join(nodes_to_update, by = c('code', 'full_name'), relationship = 'many-to-many') %>%
-  mutate(end_date = if_else(!is.na(end_date2), end_date2, end_date)) %>%
-  select(-end_date2) -> all_nodes2
+  select(-currently_valid) -> all_nodes
 
 
-
-# Some nodes need manual handling:
-# 1804 Bodo was merged with 1842 Skjerstad in 2005, but kept the code 1804 and name Bodo.
-# Same with 1103 Stavanger.
-# 0412 Ringsaker that was "split" in 1992, by giving a part to Hamar.
-# Therefore it must be a 'many-to-many' relationship in the join.
-# This creates a problem with Trondheim, since it has a more complicated history, but with
-# similar duplication as Bodo and Stavanger
-
-
-
-# Fix the end dates for 1804 Bodo and 1103 Stavanger, and Trondheim
-all_nodes2 %>%
-  mutate(end_date = if_else(code == '1804' & start_date == ymd('2005-01-01'), FINAL_DATE, end_date),
-         end_date = if_else(code == '1103' & start_date == ymd('2020-01-01'), FINAL_DATE, end_date),
-
-         # Manually fix Trondheim.
-         end_date = if_else(code == '1601' & start_date == ymd('1977-01-01'), ymd('2017-12-31'), end_date),
-         end_date = if_else(code == '5001' & start_date == ymd('2018-01-01'), ymd('2019-12-31'), end_date),
-         end_date = if_else(code == '5001' & start_date == ymd('2020-01-01'), ymd('2022-12-31'), end_date),
-
-         # Manually fix Tønsberg.
-         end_date = if_else(code == '0705' & start_date == ymd('1977-01-01'), ymd('1987-12-31'), end_date),
-         end_date = if_else(code == '0704' & start_date == ymd('1988-01-01'), ymd('2016-12-31'), end_date),
-         end_date = if_else(code == '0704' & start_date == ymd('2017-01-01'), ymd('2019-12-31'), end_date),
-         end_date = if_else(code == '3803' & start_date == ymd('2020-01-01'), ymd('2023-12-31'), end_date),
-
-         # Manually fix Ringsaker.
-         end_date = if_else(code == '0412' & start_date == ymd('1977-01-01'), ymd('1991-12-31'), end_date),
-         end_date = if_else(code == '0412' & start_date == ymd('1992-01-01'), ymd('2019-12-31'), end_date),
-
-         # Rakkestad.
-         #end_date = if_else(code == '0128' & start_date == ymd('1977-01-01'), ymd('1991-12-31'), end_date)
-         ) -> all_nodes3
+# Some checks.
+stopifnot(length(unique(all_nodes$NODE)) == nrow(all_nodes))
+stopifnot(all_nodes$NODE == 1:nrow(all_nodes))
 
 
 
@@ -147,17 +127,15 @@ all_nodes2 %>%
 
 # need to get the node numbers
 
-all_nodes3 %>%
+all_nodes %>%
   mutate(changeOccurred = end_date + 1) %>%
   select(code, changeOccurred, NODE) %>%
   rename(NODE_FROM = NODE) -> nodes_from
 
-
-all_nodes3 %>%
+all_nodes %>%
   mutate(changeOccurred = start_date) %>%
   select(code, changeOccurred, NODE) %>%
   rename(NODE_TO = NODE)-> nodes_to
-
 
 
 changetable_filtered %>%
@@ -166,36 +144,35 @@ changetable_filtered %>%
 
 
 
+
 # Do some checks.
 # Merge the edges with the node list, then check the names.
 
 # Checks the new nodes (after 1977)
-all_nodes3 %>%
+all_nodes %>%
   left_join(changetable_with_nodes, by = c('NODE' = 'NODE_TO')) %>%
   mutate(NAME_OK = full_name == newName,
          DATE_OK = changeOccurred == start_date) -> dta_node_edge_test_1
 
 
 # Checks the inital nodes (1977)
-all_nodes3 %>%
+all_nodes %>%
   left_join(changetable_with_nodes, by = c('NODE' = 'NODE_FROM')) %>%
   mutate(NAME_OK = full_name == oldName,
          DATE_OK = changeOccurred == (end_date + 1)) -> dta_node_edge_test_2
 
 
+stopifnot(all(dta_node_edge_test_1$NAME_OK, na.rm = TRUE))
+stopifnot(all(dta_node_edge_test_1$DATE_OK, na.rm = TRUE))
+stopifnot(all(dta_node_edge_test_2$NAME_OK, na.rm = TRUE))
+stopifnot(all(dta_node_edge_test_2$DATE_OK, na.rm = TRUE))
 
-# Should be TRUE
-all(dta_node_edge_test_1$NAME_OK, na.rm = TRUE)
-all(dta_node_edge_test_1$DATE_OK, na.rm = TRUE)
-all(dta_node_edge_test_2$NAME_OK, na.rm = TRUE)
-all(dta_node_edge_test_2$DATE_OK, na.rm = TRUE)
+stopifnot(all(changetable_with_nodes$NODE_FROM %in% all_nodes$NODE))
+stopifnot(all(changetable_with_nodes$NODE_TO %in% all_nodes$NODE))
 
-all(changetable_with_nodes$NODE_FROM %in% all_nodes3$NODE)
-all(changetable_with_nodes$NODE_TO %in% all_nodes3$NODE)
 
 
 ## Tidy up the data bit more ----
-
 
 # Add county names.
 
@@ -204,13 +181,13 @@ county_names_numbers <- read.csv('county_names_numbers.txt', encoding = 'UTF-8',
 fylke_parenthesis_ptrn_remove <- ' [(].+[)]$'
 
 # Only county name when duplcate kommune name
-all_nodes3 %>%
+all_nodes %>%
   mutate(full_name = str_remove(full_name, fylke_parenthesis_ptrn_remove)) %>%
 
   # Add county data
   mutate(county_num = str_sub(code, 1, 2)) %>%
   left_join(county_names_numbers, by = c('county_num' = 'county_code')) %>%
-  select(-county_num) -> all_nodes3_counties
+  select(-county_num) -> all_nodes_counties
 
 
 # Separate out norwegian, sami and kven names, from the full name.
@@ -225,7 +202,7 @@ names_sami <- readLines('samiske_navn.txt', encoding = 'UTF-8')
 names_kven <- c('Porsanki', 'Kaivuono', 'Omasvuono', 'Raisi')
 
 
-all_nodes3_counties %>%
+all_nodes_counties %>%
   #Separate out names in different languages (norwegian, sami, kven, not necessarily in that order).
   separate(full_name,
            sep = ' - ',
@@ -245,7 +222,8 @@ all_nodes3_counties %>%
          name_fkv = ifelse(name_fkv %in% names_kven, name_fkv, NA),
          name_fkv = ifelse(name_2 %in% names_kven, name_2, name_fkv),
          name_fkv = ifelse(name_3 %in% names_kven, name_3, name_fkv)) %>%
-  select(-name_1, -name_2, -name_3) -> all_nodes3_with_names
+  select(-name_1, -name_2, -name_3) -> all_nodes_with_names
+
 
 
 
@@ -260,7 +238,7 @@ changetable_with_nodes %>%
 head(dta_edges)
 
 
-all_nodes3_with_names %>%
+all_nodes_with_names %>%
   rename(id = NODE) %>%
   select(id, everything()) -> dta_nodes
 
@@ -276,7 +254,7 @@ create_graph(directed = TRUE) %>%
   add_edge_df(dta_edges) -> kommunegraph
 
 
- # render_graph(kommunegraph, layout = "fr")
+# render_graph(kommunegraph, layout = "fr")
 
 usethis::use_data(kommunegraph, overwrite = TRUE)
 
